d1a4062934f79d4c9adee017dace10574b1a7c48
Ashish Raj <ashishrahul748@gmail.com>
2024-09-08 05:54:01 +0530

    create product

--- src/controllers/admin/productController.js
+++ src/controllers/admin/productController.js
@@ -1,8 +1,73 @@
+import createHttpError from "http-errors";
+import productmodel from "../../models/productModel.js";
+import { uploadOnCloudinary } from "../../utils/cloudinary.js";
 
+const addProduct = async (req, res, next) => {
+  try {
+    const {
+      name,
+      price,
+      description,
+      category,
+      sizes,
+      colors,
+      stock,
+      brands,
+      ratings,
+      NumberOfReviews,
+      reviews,
+    } = req.body;
+    // Validate category
+    const validCategories = ["hoodies", "shirts", "pants"];
+    if (!validCategories.includes(category)) {
+      return res.status(400).json({
+        success: false,
+        message: "Please select a valid category",
+      });
+    }
 
-const addProduct =async(req,res,next)=>{
-    res.status(200).json({succes:true , message:"hello from product controller..."})
+    // Get images
+    let images = req.files.photos;
 
-}
+    if (images) {
+      for (let index = 0; index < images.length; index++) {
+        let result = await uploadOnCloudinary(images[index].path, "products");
 
-export{addProduct}
+        console.log(result);
+
+        imageArray.push({
+          id: result.public_id,
+          secure_url: result.secure_url,
+        });
+      }
+    }
+
+    //call db
+
+    const product = await productmodel.create({
+      name,
+      price,
+      description,
+      category,
+      sizes,
+      colors,
+      stock,
+      brands,
+      ratings,
+      NumberOfReviews,
+      reviews,
+      images: imageArray,
+      user: req.user.id,
+    });
+
+    res.status(200).json({
+      success: true,
+      product,
+      message: "product created successfully",
+    });
+  } catch (error) {
+    return next(createHttpError(400, "error while creating product data"));
+  }
+};
+
+export { addProduct };
